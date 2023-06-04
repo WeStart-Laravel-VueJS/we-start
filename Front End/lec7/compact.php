275c5e52cfc42e7b86089781a0d74f04a4210cc6
Mohammed Naji <m.naji1512@gmail.com>
2023-06-04 13:01:00 +0300

    lec7 - javascript

---
+++ Front End/lec7/compact.php
@@ -0,0 +1,15 @@
+
+<?php 
+$name = 'ddd';
+$age = 50;
+$username = 'ali66';
+
+compact('name', 'age', 'uername');
+
+$params = ['name', 'age', 'uername'];
+
+$finale = [];
+
+foreach($params as $pr) {
+    $finale[$pr] = $GLOBALS[$pr];
+}
